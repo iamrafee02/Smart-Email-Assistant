c552c0a46be182c48d7c1dd00344fb774e9b95e9
Rafi Ahmad <ahmadrafi025@outlook.com>
2025-08-17 15:45:12 +0530

    First Commit

---
+++ email-writer/src/test/java/com/eamil/writer/EmailWriterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eamil.writer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailWriterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
